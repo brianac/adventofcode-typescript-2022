b1936917b0a34c91bd31113387a2e7c2bc5ca905
Briana Coppard <bcoppard@zendesk.com>
2022-12-14 22:54:01 +1100

    day 13 pt 2

--- src/day13/index.ts
+++ src/day13/index.ts
@@ -1,9 +1,35 @@
 import { Day } from "../day";
 
+const compare = (left: any, right: any): any => {
+  let isLeftNum = typeof left == "number"
+  let isRightNum = typeof right == "number"
+
+  if (isLeftNum && isRightNum) {
+    return left === right ? 0 : left < right ? -1 : +1;
+  } else if (!isLeftNum && !isRightNum) {
+    const n = Math.min(left.length, right.length)
+
+    for (let i = 0; i < n; i++) {
+      const val = compare(left[i], right[i])
+      if (val !== 0) return val
+    }
+    
+    return left.length - right.length
+  } else if (isLeftNum) {
+    return compare([left], right)
+  } else if (isRightNum) {          
+    return compare(left, [right])
+  }
+}
+
 class Day13 extends Day {
 
   constructor(){
     super(13);
+  }
+
+  parse(str: string) {
+    return JSON.parse(str)
   }
 
   solveForPartOne(input: string): string {
@@ -11,37 +37,11 @@ class Day13 extends Day {
 
     let indices = 0
 
-    const parse = (str: string) => {
-      return JSON.parse(str)
-    }
-
-    const compare = (left: any, right: any): any => {
-      let isLeftNum = typeof left == "number"
-      let isRightNum = typeof right == "number"
-
-      if (isLeftNum && isRightNum) {
-        return left === right ? 0 : left < right ? -1 : +1;
-      } else if (!isLeftNum && !isRightNum) {
-        const n = Math.min(left.length, right.length)
-
-        for (let i = 0; i < n; i++) {
-          const val = compare(left[i], right[i])
-          if (val !== 0) return val
-        }
-        
-        return left.length - right.length
-      } else if (isLeftNum) {
-        return compare([left], right)
-      } else if (isRightNum) {          
-        return compare(left, [right])
-      }
-    }
-
     pairs.forEach((pair, index) => {
       const sides = pair.split('\n')
 
-      const left = parse(sides[0])
-      const right = parse(sides[1])
+      const left = this.parse(sides[0])
+      const right = this.parse(sides[1])
 
       if (compare(left, right) < 0) {
         indices+= index+1
@@ -52,7 +52,21 @@ class Day13 extends Day {
   }
 
   solveForPartTwo(input: string): string {
-    return 'input';
+    const lines = input.split('\n\n').map((a) => a.split('\n').map((b) => this.parse(b))).flat()
+
+    lines.push([[2]])
+    lines.push([[6]])
+
+    const sortedLines = lines.sort(compare)
+
+    const indexOne = sortedLines.findIndex((val) => {
+      return val.length === 1 && val[0].length === 1 && val[0][0] === 2
+    })
+    const indexTwo = sortedLines.findIndex((val) => {
+      return val.length === 1 && val[0].length === 1 && val[0][0] === 6
+    })
+
+    return ((indexOne + 1) * (indexTwo + 1)).toString();
   }
 }
 
